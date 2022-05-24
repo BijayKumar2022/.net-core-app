6da0a66eb0ad7b265c6f196aa18dbd88f4ac1f15
BijayKumar2022 <105385148+BijayKumar2022@users.noreply.github.com>
2022-05-24 15:10:27 +0530

    Views

---
+++ View/getall.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace custumcore.View
+{
+    public class getallModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
